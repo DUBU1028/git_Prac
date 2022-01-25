0794733b26a6fc05a13c07de28da5ea370cab9fb
cocosome <kcsgaram123@gmail.com>
2022-01-25 15:50:22 +0900

    edit stack.c file, add SQL_database dir, add queue.c, add list.c, add stack.exe

---
+++ c_data_structure/list.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int add_first(list, item);
+int add(list, pos, item);
+int delete(list, pos);
+int is_empty(list);
+int is_full(list);
+int display(list);
+
+int main(void)
+{
+	
+	return 0;
+}
+
+
+int add_first(list, item)
+{
+	return 0;
+}
+
+int add(list, pos, item)
+{
+	return 0;
+}
+
+int delete(list, pos)
+{
+	return 0;
+}
+
+int is_empty(list)
+{
+	return 0;
+}
+
+int is_full(list)
+{
+	return 0;
+}
+
+int display(list)
+{
+	return 0;
+}
