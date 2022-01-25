c0afa9c246339557479fab052fca88593ae57b7a
cocosome <kcsgaram123@gmail.com>
2022-01-25 19:52:51 +0900

    edit stack.c and queue.c and stack.exe, add queue.exe

--- c_data_structure/queue.c
+++ c_data_structure/queue.c
@@ -1,5 +1,82 @@
 #include <stdio.h>
+int Is_empty();
+int Is_full();
+void push(int arr[], int item);
+void pop(int arr[]);
+void Print_arr(int arr[]);
+
+int top = -1;
+int bottom = -1;
+//top = 현재 queue에 들어온 값중 가장 먼저 들어온 값의 바로 앞칸을 지정함(공백칸)
+//bottom = 현재 queue에 들어온 값중 가장 나중에 들어온(최신) 값이 있는 칸을 지정함
+
+
+//queue의 크기가 고정되어 있으며 순환되지 않는 queue를 구상함
 int main()
 {
+    int arr[10] = {0};
+    push(arr, 10);
+    push(arr, 20);
+    push(arr, 30);
+    push(arr, 40);
+    push(arr, 50);
+    push(arr, 60);
+    pop(arr);
+    pop(arr);
+    pop(arr);
+    Print_arr(arr);
     return 0;
+}
+
+int Is_empty()
+{
+    if(top == bottom)
+    return 1;
+
+    else
+    return 0;
+}
+
+//queue의 크기가 고정되어 있으므로(순환하는 queue가 아니므로) bottom의 값이 queue의 크기 - 1 값과 같다면 full이다.
+int Is_full()
+{
+    if(bottom == 9)
+    return 1;
+
+    else
+    return 0;
+}
+
+void push(int arr[], int item)
+{
+    if(Is_full() == 1)
+    {
+        printf("고정된 queue가 꽉 찼습니다.\n");
+        return ;
+    }
+    bottom = bottom + 1;
+    arr[bottom] = item;
+    printf("숫자 %d가 %d번째 칸에 push되었습니다.\n",arr[bottom], bottom);
+}
+
+void pop(int arr[])
+{
+    if(Is_empty()==1)
+    {
+        printf("고정된 queue가 비었습니다.\n");
+        return ;
+    }
+    top = top + 1;
+    int item = arr[top];
+    arr[top] = NULL;
+    printf("숫자 %d가 %d번째 칸으로 부터 pop되었습니다.\n",item, top);
+
+}
+
+void Print_arr(int arr[])
+{
+    for (int i = 0; i < 10 ; i++)
+	{
+		printf("숫자: %d, 칸: %d\n",arr[i],i);
+	}
 }
--- c_data_structure/stack.c
+++ c_data_structure/stack.c
@@ -1,1 +1,101 @@
 #include <stdio.h>
+
+int Is_empty(int top);
+int Is_full(int top);
+int push(int arr[], int top, int item);
+int pop(int arr[], int top);
+void Print_arr(int arr[]);
+
+int main(void)
+{
+	//testcase - 배열을 이용한 stack 구성하기
+	int arr[10] = {0};
+	int top = -1;
+	top = push(arr, top, 10);
+	top = push(arr, top, 15);
+	top = push(arr, top, 30);
+	top = pop(arr, top);
+	top = pop(arr, top);
+	top = pop(arr, top);
+	top = pop(arr, top);
+	top = push(arr, top, 10);
+	top = push(arr, top, 20);
+	top = push(arr, top, 30);
+	top = push(arr, top, 40);
+	top = push(arr, top, 50);
+	top = push(arr, top, 60);
+	top = push(arr, top, 70);
+	top = push(arr, top, 80);
+	top = push(arr, top, 90);
+	top = push(arr, top, 100);
+	top = push(arr, top, 200);
+	top = pop(arr, top);
+	top = pop(arr, top);
+	top = pop(arr, top);
+	top = pop(arr, top);
+	top = pop(arr, top);
+	top = pop(arr, top);
+	top = pop(arr, top);
+	top = pop(arr, top);
+	Print_arr(arr);
+	printf("This line is edited in git bash\n");
+	printf("This line is edited in git bash - 2\n");
+
+	return 0;
+}
+
+int Is_empty(int top)
+{
+	if(top == -1)
+	return 1;
+
+	else
+	return 0;
+}
+
+int Is_full(int top)
+{
+	if(top == 9)
+	return 1;
+
+	else
+	return 0;
+}
+
+int push(int arr[], int top, int item)
+{
+	if(Is_full(top) == 1)
+	{
+		printf("stack이 꽉 찼습니다.\n");
+		return top;
+	}
+	int tmp = top;
+	tmp = tmp+1;
+	arr[tmp] = item;
+	printf("숫자 %d가 %d번째 칸에 push되었습니다.\n",item, tmp);
+	return tmp;
+}
+
+int pop(int arr[], int top)
+{
+	if(Is_empty(top) == 1)
+	{
+		printf("stack이 비었습니다\n");
+		return top;
+	}
+	
+	int tmp = top;
+	int item = arr[tmp];
+	arr[tmp] = NULL;
+	printf("숫자 %d가 %d번째 칸으로 부터 pop되었습니다.\n",item, tmp);
+	tmp = tmp-1;
+	return tmp;
+}
+
+void Print_arr(int arr[])
+{
+	for (int i = 0; i < 10 ; i++)
+	{
+		printf("숫자: %d, 칸: %d\n",arr[i],i);
+	}
+}
