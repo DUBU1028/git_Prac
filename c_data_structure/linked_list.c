c14f98e8a5fd368554a6b9d699ea99e50cafb4e3
cocosome <kcsgaram123@gmail.com>
2022-02-01 16:14:46 +0900

    edit list.c

--- c_data_structure/linked_list.c
+++ c_data_structure/linked_list.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct Node{
+    struct Node *Pointer;
+    int data;
+};
 
 
 int main()
 {
+    struct Node *head = malloc(sizeof(struct Node));
     
     return 0;
 }
--- c_data_structure/list.c
+++ c_data_structure/list.c
@@ -1,45 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int add_first(list, item);
-int add(list, pos, item);
-int delete(list, pos);
-int is_empty(list);
-int is_full(list);
-int display(list);
+struct Node{
+    struct Node *Pointer;
+    int data;
+};
 
-int main(void)
+
+int main()
 {
-	
-	return 0;
-}
+    struct Node abc[3];
+    abc[0].data = 0;
+    abc[0].Pointer = &abc[1];
 
+    abc[1].data = 10;
+    abc[1].Pointer = &abc[2];
 
-int add_first(list, item)
-{
-	return 0;
-}
+	// abc[2].data = 20;
+	// abc[2].Pointer = NULL;
 
-int add(list, pos, item)
-{
-	return 0;
-}
-
-int delete(list, pos)
-{
-	return 0;
-}
-
-int is_empty(list)
-{
-	return 0;
-}
-
-int is_full(list)
-{
-	return 0;
-}
-
-int display(list)
-{
-	return 0;
+	printf("0번칸의 주소: %p, 0번칸의 data값: %d, 0번칸의 pointer가 가르키는 주소: %p\n",&abc[0], abc[0].data, *abc[0].Pointer);
+	printf("1번칸의 주소: %p, 1번칸의 data값: %d, 1번칸의 pointer가 가르키는 주소: %p\n",&abc[1], abc[1].data, *abc[1].Pointer);
+	// printf("2번칸의 주소: %p, 2번칸의 data값: %d, 2번칸의 pointer가 가르키는 주소: %p\n",&abc[2], abc[2].data, *abc[2].Pointer);
+	printf("배열의 주소: %p\n",abc);
+    return 0;
 }
